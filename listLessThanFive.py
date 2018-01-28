1964a63542f3b3d83a841e5a223609fe8dde3941
Mugabe <35006498+MugabeButler@users.noreply.github.com>
2018-01-27 22:23:31 -0500

    Add files via upload

---
+++ listLessThanFive.py
@@ -0,0 +1,10 @@
+#prints list values in a that are less than 5
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for lessthanfive in a:
+    if lessthanfive < 5:
+        print(lessthanfive)
+    else:
+        break
+    
+    
